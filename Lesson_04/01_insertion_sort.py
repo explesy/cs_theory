62100a02a4982bbefd443b7b6f7354da37927277
Andrew F <explesy@gmail.com>
2014-09-17 00:38:50 +0400

    Update selection sort algorithm

--- Lesson_04/01_insertion_sort.py
+++ Lesson_04/01_insertion_sort.py
@@ -5,7 +5,6 @@ def insertion_sort(array):
 	for i in range(1, len(array)):
 		k = i 
 		while k > 0 and array[k] < array[k-1]:
-			print(array)
 			array[k], array[k-1] = array[k-1], array[k]
 			k -= 1
 
@@ -15,7 +14,7 @@ def insertion_sort(array):
 def main():
 	test = [12, 24, 6456, 13, 256, 1, 6, 786, 3543]
 
-	# print(test)
+	print(test)
 	print("\n{}  <-- sorted".format(insertion_sort(test)))
 
 
--- Lesson_04/02_selection_sort.py
+++ Lesson_04/02_selection_sort.py
@@ -7,9 +7,7 @@ def selection_sort(array):
 		for k in range(i, len(array)):
 			if array[k] < array[min]:
 				min = k
-		print(array)
-		min_elt = array.pop(min)
-		array.insert(i, min_elt)
+		array[i], array[min] = array[min], array[i]
 
 	return array
 
@@ -17,7 +15,7 @@ def selection_sort(array):
 def main():
 	test = [12, 24, 6456, 13, 256, 1, 6, 786, 3543]
 
-	# print(test)
+	print(test)
 	print("\n{}  <-- sorted".format(selection_sort(test)))
 
 
